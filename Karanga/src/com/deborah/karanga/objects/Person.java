7a7534ac679b9b66ff68d9d463e78b7deeca7086
absidibe <ali@followanalytics.com>
2018-03-14 23:37:08 +0100

    Ajouter des modification en class personne

--- Karanga/src/com/deborah/karanga/objects/Person.java
+++ Karanga/src/com/deborah/karanga/objects/Person.java
@@ -3,7 +3,68 @@ package com.deborah.karanga.objects;
 import java.util.Date;
 
 public class Person {
-private String firstName;
-private String lastName;
-private Date dateOfBirth;
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Date   dateOfBirth;
+    private String city;
+    private String cityzenship;
+    private String employment;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCityzenship() {
+        return cityzenship;
+    }
+
+    public void setCityzenship(String cityzenship) {
+        this.cityzenship = cityzenship;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmployment() {
+        return employment;
+    }
+
+    public void setEmployment(String employment) {
+        this.employment = employment;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
